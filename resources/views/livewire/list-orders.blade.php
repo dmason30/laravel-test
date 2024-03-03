a07349afa710097fd07ae75ecd691155e1bddb13
dmason30 <danmason@spendology.com>
2024-03-03 22:12:10 +0000

    Add product and sold at to orders table

--- resources/views/livewire/list-orders.blade.php
+++ resources/views/livewire/list-orders.blade.php
@@ -3,16 +3,20 @@
 
     <table class="w-full bg-white border border-collapse border-gray-300 rounded-md text-left">
         <thead>
+            <th class="border border-gray-300 p-2">{{ __('Product') }}</th>
             <th class="border border-gray-300 p-2">{{ __('Quantity') }}</th>
             <th class="border border-gray-300 p-2">{{ __('Unit Cost') }}</th>
             <th class="border border-gray-300 p-2">{{ __('Selling Price') }}</th>
+            <th class="border border-gray-300 p-2">{{ __('Sold At') }}</th>
         </thead>
         <tbody>
             @foreach($orders as $order)
                 <tr>
+                    <td class="border border-gray-300 p-2">{{$order->product->name}}</td>
                     <td class="border border-gray-300 p-2">{{$order->quantity}}</td>
                     <td class="border border-gray-300 p-2">{{$order->unit_cost->format()}}</td>
                     <td class="border border-gray-300 p-2">{{$order->total_charge->format()}}</td>
+                    <td class="border border-gray-300 p-2">{{$order->created_at->format('Y-m-d H:i')}}</td>
                 </tr>
             @endforeach
         </tbody>
